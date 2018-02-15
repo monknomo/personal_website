7181c167bd127b9746b6fb0bfc813e6ebab75b4b
monknomo <monknomo@gmail.com>
2018-02-14 21:55:09 -0900

    ssl conf

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://gunnargissel.com'
+SITEURL = 'https://www.gunnargissel.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
